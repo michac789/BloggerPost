7997278c265a111b3e533dd9da534074d1b34487
Michael Andrew Chan <67429929+michac789@users.noreply.github.com>
2022-07-29 17:45:29 +0800

    added ejs, connect to mongoose

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
+// load express, path, define port number
 const express = require('express')
 const app = express()
+const path = require('path')
+const PORT = 3100
 
-PORT = 3100
+// load our models
+const BlogPost = require('./models/blog')
+
+// connect to mongoose
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:27017/blogpost', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+// load views & static folder
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, '/views'))
+app.use(express.static('static'))
 
 app.get('/', (req, res) => {
-    res.send("<h1>Hello World</h1>")
+    res.render('blog/home')
 })
 
 app.listen(PORT, () => {
--- models/blog.js
+++ models/blog.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const BlogPost = new Schema({
+const blogSchema = new Schema({
     title: String,
     content: String,
 })
+
+module.exports = mongoose.model('BlogPost', blogSchema)
