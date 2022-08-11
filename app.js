3840107a042faf5ef073cb29a2c470bf9285b4ef
Michael Andrew Chan <67429929+michac789@users.noreply.github.com>
2022-08-11 15:19:22 +0800

    added env var and basic cloudinary setup & 3 new modules

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+if (process.env.NODE_ENV !== "production") {
+    require('dotenv').config()
+}
+console.log(process.env.SOMEKEY)
+
 // load express, path, define port number
 const express = require('express')
 const app = express()
--- routes/blog.js
+++ routes/blog.js
@@ -5,12 +5,22 @@ const asyncWrap = require('../utils/asyncWrap')
 const { loginRequired } = require('../middlewares/loginRequired')
 const Blog = require('../controllers/blog')
 
+const multer = require('multer')
+const { storage } = require('../cloudinary')
+const upload = multer({ storage })
+
 
 router.get('', asyncWrap(Blog.viewAll))
 
 router.route('/create')
     .get(loginRequired, Blog.createForm)
-    .post(loginRequired, asyncWrap(Blog.createDo))
+    .post(loginRequired, 
+        upload.single('image'),
+        (req, res) => {
+            console.log(req.body, req.file)
+        }
+        //asyncWrap(Blog.createDo)
+        )
 
 router.route('/:id')
     .get(asyncWrap(Blog.viewBlog))
